085cb06c6b79d732424b262b26a0749cef37521e
JoannaPoplawska <joannapop92@gmail.com>
2020-12-09 20:37:22 +0100

    Utworzenie klasy bazowej - HelperBase

--- adressbook-web-tests/src/test/java/ru/stqa/pfr/adressbook/appmanager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pfr/adressbook/appmanager/GroupHelper.java
@@ -1,43 +1,42 @@
 package ru.stqa.pfr.adressbook.appmanager;
 
+import com.sun.javafx.binding.ExpressionHelperBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pfr.adressbook.tests.GroupData;
 
-public class GroupHelper {
-    private WebDriver wd;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+
+        super(wd);
     }
 
     public void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
+        click(By.linkText("group page"));
     }
 
     public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void fillGroupFrom(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).clear();
+        type(By.name("group_name"), groupData.getName());
+       // wd.findElement(By.name("group_header")).clear();
         wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).clear();
+       // wd.findElement(By.name("group_footer")).clear();
         wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     public void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
+        click(By.name("new"));
     }
 
     public void deleteSelectedGroups() {
-        wd.findElement(By.name("delete")).click();
+        click(By.name("delete"));
     }
 
     public void selectGroup() {
-        wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 }
--- adressbook-web-tests/src/test/java/ru/stqa/pfr/adressbook/appmanager/SessionsHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pfr/adressbook/appmanager/SessionsHelper.java
@@ -3,22 +3,16 @@ package ru.stqa.pfr.adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionsHelper {
-    private WebDriver wd;
+public class SessionsHelper extends HelperBase {
 
     public SessionsHelper(WebDriver wd) {
 
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//input[@value='Login']"));
     }
 }
