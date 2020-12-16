316cb00c83f98fc5cfb63b1176c20affc2afc807
JoannaPoplawska <joannapop92@gmail.com>
2020-12-16 23:55:06 +0100

    Test do równania kwadratowego

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pfr/adressbook/tests/ContactCreationTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.pfr.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pfr.adressbook.model.TestBase;
+
+public class ContactCreationTests extends TestBase {
+    @Test
+    public void testContactCreation(){
+
+    }
+}
